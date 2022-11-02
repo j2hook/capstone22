8bdbe117ec0650770595499f5ade76ff2e7dbff0
Jinhyeong <j2hook@gmail.com>
2022-11-02 16:51:02 +0900

    2022.11.02 웹 게임 브라우저에 구동 완료

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
-// import './App.css';
-// import React from 'react';
-// import { Unity, useUnityContext } from "react-unity-webgl";
-
-// function App() {
-
-//   const { unityProvider } = useUnityContext({
-//     loaderUrl: "unity_build/Snake/Build/Snake.loader.js",
-//     dataUrl: "unity_build/Snake/Build/Snake.data",
-//     frameworkUrl: "unity_build/Snake/Build/Snake.framework.js.",
-//     codeUrl: "unity_build/Snake/Build/Snake.wasm",
-//     webglContextAttributes: {
-//       preserveDrawingBuffer: true,
-//     },
-//   });
-
-//   return (
-//     <div>
-//     <h1>react test</h1>
-//     <Unity unityProvider={unityProvider} />
-//     </div>
-//   );
-// }
 
 import React, { Fragment } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
+
 function App() {
   const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-    loaderUrl: "unity_build/Snake3/Build/Snake3.loader.js",
-    dataUrl: "unity_build/Snake3/Build/Snake3.data",
-    frameworkUrl: "unity_build3/Snake/Build/Snake3.framework.js.",
-    codeUrl: "unity_build/Snake3/Build/Snake3.wasm",
+    loaderUrl: "unity_build/Snake4/Build/unity.loader.js",
+    dataUrl: "unity_build/Snake4/Build/unity_build.data",
+    frameworkUrl: "unity_build/Snake4/Build/unity_build.framework.js",
+    codeUrl: "unity_build/Snake4/Build/unity_build.wasm",
   });
   return (
     <Fragment>
