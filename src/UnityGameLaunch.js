d28dafe52e2b540fa5af66eb365ec21be7b56284
Jinhyeong <j2hook@gmail.com>
2022-11-28 00:48:35 +0900

    2022.11.28 게임 업로드 밑 html 수정

--- src/UnityGameLaunch.js
+++ src/UnityGameLaunch.js
@@ -1,23 +1,23 @@
-// import React, { Fragment } from "react";
-// import { Unity, useUnityContext } from "react-unity-webgl";
+import React, { Fragment } from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
-// export default function UnityGameLaunch(name) {
+export default function UnityGameLaunch(name) {
 
-//     const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-//         loaderUrl: "unity_build/"+name+"/Build/"+name+".js",
-//         dataUrl: "unity_build/"+name+"/Build/"+name+".data",
-//         frameworkUrl: "unity_build/"+name+"/Build/"+name+".framework.js",
-//         codeUrl: "unity_build/"+name+"/Build/"+name+".wasm",
-//       });
-//       return (
-//         <Fragment>
-//           {!isLoaded && (
-//             <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
-//           )}
-//           <Unity
-//             unityProvider={unityProvider}
-//             style={{ visibility: isLoaded ? "visible" : "hidden" }}
-//           />
-//         </Fragment>
-//       );
-// };
+    const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+        loaderUrl: "unity_build/"+name+"/Build/"+name+".js",
+        dataUrl: "unity_build/"+name+"/Build/"+name+".data",
+        frameworkUrl: "unity_build/"+name+"/Build/"+name+".framework.js",
+        codeUrl: "unity_build/"+name+"/Build/"+name+".wasm",
+      });
+      return (
+        <Fragment>
+          {!isLoaded && (
+            <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
+          )}
+          <Unity
+            unityProvider={unityProvider}
+            style={{ visibility: isLoaded ? "visible" : "hidden" }}
+          />
+        </Fragment>
+      );
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import UnityGameLaunch from './UnityGameLaunch';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const unity = ReactDOM.createRoot(document.getElementById('unity'));
+unity.render(
   <React.StrictMode>
-    <App />
+    <UnityGameLaunch />
   </React.StrictMode>
-);
-
-reportWebVitals();
+)
